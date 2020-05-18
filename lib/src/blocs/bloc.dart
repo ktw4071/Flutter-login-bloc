cbdeb94fa22a08f3449cab51d28cb18489afba98
Taewoo Kim <taewkim@cisco.com>
2020-05-18 00:22:00 -0400

    adding block getter

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -3,4 +3,6 @@ import 'dart:async';
 class Bloc {
   final emailController = StreamController<String>();
   final passwordController = StreamController<String>();
+
+  Function(String) get changeEmail => emailController.sink.add;
 }
