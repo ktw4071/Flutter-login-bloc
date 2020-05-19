ef927c384f065b16d9357c861727be098a549741
Taewoo Kim <taewkim@cisco.com>
2020-05-18 23:29:54 -0400

    adding rxdart

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Bloc with Validators {
   final _email = StreamController<String>();
@@ -8,6 +9,7 @@ class Bloc with Validators {
   // Add data to stream
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
+  Stream<bool> get submitValid => Rx.combineLatest2(email, password, (a, b) => true);
 
   // change data
   Function(String) get changeEmail => _email.sink.add;
