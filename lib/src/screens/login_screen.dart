53d1cfdbf0fe599008ef82c37877e774b1ab40ee
Taewoo Kim <taewkim@cisco.com>
2020-05-18 23:44:04 -0400

    adding a combineStream in submit button

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -14,7 +14,7 @@ class LoginScreen extends StatelessWidget {
           Container(
             margin: EdgeInsets.only(top: 25.0)
           ),
-          submitButton()
+          submitButton(bloc),
         ],
       )
     );
@@ -54,11 +54,18 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget submitButton() {
-    return RaisedButton(
-      child: Text('Login'),
-      color: Colors.blue,
-      onPressed: () {},
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder<Object>(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text('Login'),
+          color: Colors.blue,
+          onPressed: snapshot.hasError ? null : () {
+            print('Hi there!');
+          },
+        );
+      }
     );
   }
 }
